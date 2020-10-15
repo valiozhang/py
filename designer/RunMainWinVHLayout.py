40cd899727c811dd4ef2b4301369594eada9df35
valiozhang <btbuzhangtianyu@163.com>
2020-10-15 15:55:19 +0800

    VHLayout

---
+++ designer/RunMainWinVHLayout.py
@@ -0,0 +1,17 @@
+import sys
+import MainWinVHLayout
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)#创建Qapplication对象表示应用程序
+    mainWindow = QMainWindow()#创建主窗口
+    ui = MainWinVHLayout.Ui_MainWindow()#引用自动生成的Ui_MainWindow类
+    ui.setupUi(mainWindow)#调用setuiUi方法，向主窗口添加控件
+    mainWindow.show()
+    sys.exit(app.exec_())#运行主循环
+
+
+
+
+
