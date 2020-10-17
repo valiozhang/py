2e32c0c657ac3f81746ca5357a0a2fa200f2e194
valiozhang <btbuzhangtianyu@163.com>
2020-10-17 12:19:54 +0800

    screengeometry

--- controls/ScreenGeometry.py
+++ controls/ScreenGeometry.py
@@ -18,8 +18,8 @@ def onClick_Button():
     print("方法三")  #
     print("widget.frameGeometry().x() = %d" % widget.frameGeometry().x()) #250 （窗口（含有标题栏）的左上角横坐标）
     print("widget.frameGeometry().y() = %d" % widget.frameGeometry().y()) #200 （窗口（含有标题栏）的左上角纵坐标）
-    print("widget.frameGeometry().width() = %d" % widget.frameGeometry().width())  #302 (窗口宽度)
-    print("widget.frameGeometry().height() = %d" % widget.frameGeometry().height())  #293 （窗口高度）
+    print("widget.frameGeometry().width() = %d" % widget.frameGeometry().width())  #302 (窗口（包含了标题栏和边框）宽度)
+    print("widget.frameGeometry().height() = %d" % widget.frameGeometry().height())  #293 窗口（包含了标题栏和边框）高度）
 
 #使用面向过程的方式
 app = QApplication(sys.argv)
