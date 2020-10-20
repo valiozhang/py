4aa1f6d4d818616de3c6acb2f2c44bffba00a397
valiozhang <btbuzhangtianyu@163.com>
2020-10-20 18:39:12 +0800

    QCheckBoxDemo

--- controls/QCheckBoxDemo.py
+++ controls/QCheckBoxDemo.py
@@ -23,9 +23,11 @@ class QCheckBoxDemo(QWidget):
         self.setWindowTitle('复选框控件演示')
         layout = QHBoxLayout() # 水平布局
 
+        # 让复选框处于选中/未选中状态
         self.checkBox1 = QCheckBox('复选框控件1')
         self.checkBox1.setChecked(True) # 默认选中
-        self.checkBox1.stateChanged.connect(lambda:self.checkboxState(self.checkBox1)) # stateChanged是状态变化
+        # stateChanged：出现状态变化就会产生信号
+        self.checkBox1.stateChanged.connect(lambda:self.checkboxState(self.checkBox1))
         layout.addWidget(self.checkBox1)
 
         self.checkBox2 = QCheckBox('复选框控件2')
@@ -35,7 +37,7 @@ class QCheckBoxDemo(QWidget):
         self.checkBox3 = QCheckBox('半选中')
         self.checkBox3.stateChanged.connect(lambda:self.checkboxState(self.checkBox3))
         self.checkBox3.setTristate(True)                    # 设置真才能够处于半选中状态
-        self.checkBox3.setCheckState(Qt.PartiallyChecked)   # 设置半选中状态
+        self.checkBox3.setCheckState(Qt.PartiallyChecked)   # setCheckState：描述当前复选框处于哪种状态
         layout.addWidget(self.checkBox3)
 
         self.setLayout(layout)
