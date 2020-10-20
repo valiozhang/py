159e91a4e038a62509b8efcfd824996e01a13314
valiozhang <btbuzhangtianyu@163.com>
2020-10-20 18:35:54 +0800

    QCheckBoxDemo

---
+++ controls/QCheckBoxDemo.py
@@ -0,0 +1,54 @@
+"""
+复选框控件，在pyqt5中复选框具有三种状态：
+
+未选中：0
+半选中：1
+选中：    2
+但是对于半选中状态，只有在 .setTristate(True) 设置之后才可以设置复选框选项的半选中状态。
+
+"""
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+
+class QCheckBoxDemo(QWidget):
+    def __init__(self):
+        super(QCheckBoxDemo,self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('复选框控件演示')
+        layout = QHBoxLayout() # 水平布局
+
+        self.checkBox1 = QCheckBox('复选框控件1')
+        self.checkBox1.setChecked(True) # 默认选中
+        self.checkBox1.stateChanged.connect(lambda:self.checkboxState(self.checkBox1)) # stateChanged是状态变化
+        layout.addWidget(self.checkBox1)
+
+        self.checkBox2 = QCheckBox('复选框控件2')
+        self.checkBox2.stateChanged.connect(lambda:self.checkboxState(self.checkBox2))
+        layout.addWidget(self.checkBox2)
+
+        self.checkBox3 = QCheckBox('半选中')
+        self.checkBox3.stateChanged.connect(lambda:self.checkboxState(self.checkBox3))
+        self.checkBox3.setTristate(True)                    # 设置真才能够处于半选中状态
+        self.checkBox3.setCheckState(Qt.PartiallyChecked)   # 设置半选中状态
+        layout.addWidget(self.checkBox3)
+
+        self.setLayout(layout)
+
+    def checkboxState(self,cb):
+        check1Status = self.checkBox1.text() + ', isChecked=' + str(self.checkBox1.isChecked()) + ',checkState=' + str(self.checkBox1.checkState()) + '\n'
+        check2Status = self.checkBox2.text() + ', isChecked=' + str(self.checkBox2.isChecked()) + ',checkState=' + str(self.checkBox2.checkState()) + '\n'
+        check3Status = self.checkBox3.text() + ', isChecked=' + str(self.checkBox3.isChecked()) + ',checkState=' + str(self.checkBox3.checkState()) + '\n'
+        print(check1Status + check2Status + check3Status)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QCheckBoxDemo()
+    main.show()
+    sys.exit(app.exec_())
